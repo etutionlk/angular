b6a695979982e92cf75c65fda7346f710456ad22
Chinthaka MAduranga <chinthaka.maduranga@blackswan-technologies.com>
2021-03-19 08:27:38 +0530

    intial module creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartModule } from './cart/cart.module';
+import { FrontendModule } from './frontend/frontend.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./frontend/frontend.module').then(m => FrontendModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./cart/cart.module').then(m => CartModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TopheaderComponent } from './topheader/topheader.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TopheaderComponent, HeaderComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TopheaderComponent
   ]
 })
+
 export class FrontendModule { }
